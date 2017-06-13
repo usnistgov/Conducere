b9b4410341f2cdaf021a65a1cbec9725d483d76e
lizhong <lizhong.zhang@nist.gov>
2017-06-13 15:53:57 -0400

    Remove commented code in install.sh

--- image/install.sh
+++ image/install.sh
@@ -4,10 +4,6 @@
 sleep 10
 
 # Install Ansible
-#sudo apt-get update
-#sudo apt-get -y install python-dev python-pip
-#sudo pip install ansible
-
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo apt-add-repository ppa:ansible/ansible
